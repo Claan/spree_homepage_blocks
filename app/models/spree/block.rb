3ff35815bb6efc3830b55bb0ef64100a947d8b7f
Nuno Polonia <nuno@claan.com>
2014-11-07 15:34:24 +0000

    Add default scope to blocks

--- app/models/spree/block.rb
+++ app/models/spree/block.rb
@@ -2,6 +2,8 @@ class Spree::Block < ActiveRecord::Base
   validates_presence_of :link
   validates_presence_of :image
 
+  default_scope -> { order("position ASC") }
+
   has_attached_file :image, 
      styles: {mini: '48x48#', normal: '200x200>', large: '600x600>'},
      default_style: :large,
