f4562f3a013e3c803ce53df74591cd00780b1035
Nuno Polonia <nuno@claan.com>
2014-11-12 10:52:39 +0000

    Create blocks with bootstrap classes

--- app/overrides/add_blocks_to_homepage.rb
+++ app/overrides/add_blocks_to_homepage.rb
@@ -1,5 +1,5 @@
 Deface::Override.new(virtual_path:  "spree/home/index",
                      name:          "blocks_homepage",
                      replace: "[data-hook='homepage_products']",
-                     text:          "<div class='homepage_blocks'><%= render @blocks %></div>",
+                     text:          "<div class='homepage_blocks col-sm-12 col-md-12'><%= render @blocks %></div>",
                      disabled:      false)
